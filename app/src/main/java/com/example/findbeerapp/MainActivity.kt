525ed2394c16bde6a78aa6edae79949ca9dd2388
Robert1996s <54981029+Robert1996s@users.noreply.github.com>
2020-05-13 10:01:02 +0200

    Firebase connect, more work

--- app/src/main/java/com/example/findbeerapp/MainActivity.kt
+++ app/src/main/java/com/example/findbeerapp/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.findbeerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+// Create a new user with a first and last name
+
+        // Create a new user with a first and last name
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Ada"
+        user["last"] = "Lovelace"
+        user["born"] = 1815
+
+// Add a new document with a generated ID
+        
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    "!!!",
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e -> Log.w("!!!", "Error adding document", e) }
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.beer_list)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = BarRecycleAdapter(this, DataManager.bars)
+
+        recyclerView.adapter = adapter
     }
 }

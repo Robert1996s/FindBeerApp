973ba0fb02ccdec6d535c1b671e12900d5220855
Robert1996s <54981029+Robert1996s@users.noreply.github.com>
2020-05-27 13:58:27 +0200

    Map location fix, pins and list order

--- app/src/main/java/com/example/findbeerapp/MapsActivity.kt
+++ app/src/main/java/com/example/findbeerapp/MapsActivity.kt
@@ -1,18 +1,29 @@
 package com.example.findbeerapp
 
+
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener,
+    GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
+
+    //private lateinit var originPosition: Point
+    //private lateinit var destinationPosition: Point
+    //private lateinit var destinationMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,24 +32,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
         val mapView = findViewById<View>(R.id.map_display)
 
 
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        /*val mapFragment = supportFragmentManager
+        val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map_display) as SupportMapFragment
-        mapFragment.getMapAsync(this)*/
+        mapFragment.getMapAsync(this)
     }
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.setOnInfoWindowClickListener(this)
+        mMap.isMyLocationEnabled = true
+
+        //Listens to map clicks
+        mMap.setOnMapClickListener {
+            mMap.addMarker(MarkerOptions().position(it))
+
+        }
         // Add a marker in Sydney and move the camera
         val stockholm = LatLng(59.517, 17.916)
-        val marker = mMap.addMarker(MarkerOptions()
-            .position(stockholm)
-            .title("Stockholm")
-            .snippet("Vackert här!"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm))
+        val marker = mMap.addMarker(
+            MarkerOptions()
+                .position(stockholm)
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stockholm, 12.0f))
     }
+
     /*fun createLocationRequest() : LocationRequest {
     var a = LocationRequest.create()
     a.interval = 7000
@@ -49,4 +70,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
     override fun onInfoWindowClick(marker: Marker?) {
         Toast.makeText(this, "Här är du", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onMarkerClick(point: Marker?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+
 }
